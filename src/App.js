e16d9e0939862d006fe79ded5a13e6e318732a47
Chhaya1602 <krushikirtimandlik@gmail.com>
2024-08-22 21:37:44 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import TempControl from './Components/TempControl'
+
+function App() {
+  return (
+    <div className="App">
+     { <TempControl/>  }
+      
+    </div>
+  );
+}
+
+export default App;
